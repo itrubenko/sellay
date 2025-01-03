258b338aa69e25d67bdb69c12f50ba3c470f3847
832 <igor.trubenko@contractor.hallmark.com>
2025-01-04 00:20:35 +0200

    Added MongDB logic

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+(function(){
+    const profileForm = document.getElementById('profile');
+    $(profileForm).on('submit', async function (e) {
+        e.preventDefault();
+        var formData = new FormData(profileForm);
+        let request = await fetch('http://localhost:3000/update', {
+            method: 'POST',
+            body: formData
+        });
+
+        var result = request.json();
+        console.log(result);
+    });
+
+    fetch('http://localhost:3000/profile')
+        .then(data => data.json())
+        .then(data => {
+            profileForm.name.value = data.name;
+            profileForm.surname.value = data.surname;
+        });
+})($);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://admin:password@localhost:27017";
+const client = new MongoClient(uri);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{ title: 'Express 1' });
+    res.render('index',{ title: 'Express 1' });
+});
+
+/* GET home page. */
+router.get('/profile', function(req, res) {
+    const eventName = "serverOpening";
+    client.on(eventName, event => {
+        console.log(`received ${eventName}: ${JSON.stringify(event, null, 2)}`);
+    });
+
+    async function run() {
+        try {
+            await client.connect();
+
+            let db = client.db("user-account");
+            let collection = await db.collection("users").findOne();
+            await client.close();
+            res.send(collection);
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+
+router.post('/update', function(req, res) {
+    async function run() {
+        try {
+            await client.connect();
+            let query = { UserID: 1 };
+            let db = client.db("user-account");
+            let newValues = { $set: req.body };
+            let result = await db.collection("users").updateOne(query, newValues);
+            await client.close();
+            res.send(result);
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
 });
 
 module.exports = router;
